96d45d7425aa2e8d877ba008888483ae1a528466
PilarIESCampanillas <47025246+PilarIESCampanillas@users.noreply.github.com>
2021-11-11 10:45:02 +0100

    primera version de halloweeen

---
+++ javascripts/halloween.js
@@ -0,0 +1,17 @@
+/*********** javascripts de la presentación de halloween  ********/
+
+function mostrar(imagen){
+    document.getElementById(imagen).style.visibility="visible";
+}
+
+function ocultar(imagen){
+    document.getElementById(imagen).style.visibility="hidden";
+}
+
+function sonar(sonido){
+    document.getElementById(sonido).play();
+}
+
+function pausar(sonido){
+    document.getElementById(sonido).pause();
+}
